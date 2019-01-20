a869864e31b2b7e08c9ada7c8c6f3d324d1a52d9
JamesACM <jaimecanalesmarin@gmail.com>
2019-01-20 09:00:46 -0300

    feat: remove comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,6 @@
 class Article < ApplicationRecord
   self.per_page = 10
   belongs_to :district,inverse_of: :articles
-  #has_one :region, through: :district
   validates_presence_of :title
   validates :description, presence: true, length: { in: 3..300 , message: 'mínimo 3 máximo 300 caracteres'}
 
--- app/models/district.rb
+++ app/models/district.rb
@@ -1,5 +1,4 @@
 class District < ApplicationRecord
   belongs_to :province, inverse_of: :districts
   has_many :articles, inverse_of: :districts
-  #has_one :region, through: :province
 end
